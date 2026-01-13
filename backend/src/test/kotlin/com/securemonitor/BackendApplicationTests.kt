1e2e7a498584f5600c24ed41873bd47eb6cba49a
Daniel Parker <daniel.a.b.parker@gmail.com>
2026-01-13 13:27:39 +0100

    feat: add Spring Boot backend project with Kotlin

---
+++ backend/src/test/kotlin/com/securemonitor/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.securemonitor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
